1af12f5ff73a9a4e255482663b8c521629296542
Phong <phongpvse161146@fpt.edu.vn>
2026-03-04 13:35:57 +0700

    fix  axiosinsstance

--- fe_fload_admin/vite.config.js
+++ fe_fload_admin/vite.config.js
@@ -1,32 +1,12 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-
-// export default defineConfig({
-//   plugins: [react()],
-
-//   base: "./", // 👈 thêm dòng này cho production
-
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// });
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
-  base: "./",   // 👈 QUAN TRỌNG NHẤT
-
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api-rescue.purintech.id.vn',
-        changeOrigin: true,
-      },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
